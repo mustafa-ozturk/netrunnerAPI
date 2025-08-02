3eb1a480c20af4ee73a784f7fc6c708022b6c2bc
Mustafa Ozturk <68792671+mustafa-ozturk@users.noreply.github.com>
2025-08-01 20:12:20 -0400

    core: separate express app from server startup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import postgres from "postgres";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle } from "drizzle-orm/postgres-js";
-import express from "express";
-
-import { middlewareErrorHandler } from "./middleware.js";
+import { app } from "./app.js";
 import { config } from "./config.js";
-
-// automatic migrations
-const migrationClient = postgres(config.db.url, { max: 1 });
-await migrate(drizzle(migrationClient), config.db.migrationConfig);
-
-const app = express();
-
-app.use(middlewareErrorHandler);
 
 app.listen(config.api.port, () => {
   console.log(`Server is running at http://localhost:${config.api.port}`);
