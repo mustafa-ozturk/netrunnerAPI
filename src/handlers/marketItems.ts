ef0e24aee28554c89de4bf3e5cb8d9b29051ae1d
Mustafa Ozturk <68792671+mustafa-ozturk@users.noreply.github.com>
2025-08-12 14:51:17 -0400

    fix: user can't sell item they don't own (#34)

--- src/handlers/marketItems.ts
+++ src/handlers/marketItems.ts
@@ -4,6 +4,7 @@ import { config } from "../config.js";
 import { createMarketItems } from "../db/queries/marketItems.js";
 import { BadRequestError } from "../error.js";
 import { respondWithJSON } from "../json.js";
+import { getItemsByUserId } from "../db/queries/items.js";
 
 export const handlerAddItemToMarket = async (req: Request, res: Response) => {
   const token = getBearerToken(req);
@@ -12,6 +13,21 @@ export const handlerAddItemToMarket = async (req: Request, res: Response) => {
 
   if (!itemIds) {
     throw new BadRequestError("itemIds required");
+  }
+
+  // check if items owned by user
+  const userOwnedItems = await getItemsByUserId(userId);
+  if (!userOwnedItems) {
+    throw new Error("couldn't get items");
+  }
+  const userOwnedItemIds = userOwnedItems.map((item) => item.id);
+
+  const allItemsSoldOwnedByUser = itemIds.every((itemId: string) =>
+    userOwnedItemIds.includes(itemId)
+  );
+
+  if (!allItemsSoldOwnedByUser) {
+    throw new BadRequestError("Found items not owned by the user.");
   }
 
   const marketItems = await createMarketItems(
